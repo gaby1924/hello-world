55b89fd858514b704a3adcffd89bbaac58eedb35
Gaby Kaul <gkaul13@gmail.com>
2026-05-02 21:13:21 -0400

    Update navigation links to use relative paths

--- includes/header.php
+++ includes/header.php
@@ -15,10 +15,10 @@ if (session_status() == PHP_SESSION_NONE) {
 <header>
     <h2>My Shopping Cart</h2>
     <nav>
-    <a href="/shopping_cart/public/index.php?page=home">Home</a> |
-    <a href="/shopping_cart/public/index.php?page=catalog">Catalog</a> |
-    <a href="/shopping_cart/public/index.php?page=cart">Cart</a> |
-    <a href="/shopping_cart/public/index.php?page=checkout">Checkout</a>
+        <a href="index.php?page=home">Home</a> |
+        <a href="index.php?page=catalog">Catalog</a> |
+        <a href="index.php?page=cart">Cart</a> |
+        <a href="index.php?page=checkout">Checkout</a>
 </nav>
     <hr>
 </header>
